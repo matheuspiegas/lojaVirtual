92b2bce516af5e4c8676920950fdd317b082a0e5
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-06-09 20:49:28 -0300

    adicionando rotas para dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,9 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TypesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductsController::class, 'getAvailableProducts']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [ProductsController::class, 'indexDashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
