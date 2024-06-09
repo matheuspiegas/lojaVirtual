2e771f3e3cde0b6f03f48bfe6582dab5cca8578c
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-06-09 20:50:19 -0300

    criando componente de navegação

---
+++ resources/views/components/navigationdefault.blade.php
@@ -0,0 +1,24 @@
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    </a>
+                </div>
+            </div>
+
+            <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                    {{ __('Login') }}
+                </x-nav-link>
+                <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                    {{ __('Registrar') }}
+                </x-nav-link>
+
+            </div>
+        </div>
+</nav>
