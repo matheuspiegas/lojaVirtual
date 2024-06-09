6aa25d2b08d70ac0d72a2135cdd69665c42c4b63
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-06-09 20:50:47 -0300

    criando layout para rota pública de listagem de produtos disponíveis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,52 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-700">
+    <x-navigationdefault />
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <!-- Conteúdo da página -->
-                <p>Bem-vindo a minha loja virtual!</p>
-                <a href="{{ url('login') }}">Login</a><br><br>
-                <a href="{{ url('register') }}">Registro</a><br><br>
-                <p>Aqui a lista dos meus produtos!</p>
+            <div class="overflow-hidden ">
+                <h1 class="text-4xl mb-3">Produtos disponíveis</h1>
+                <div class="grid  rounded-md  grid-cols-4 gap-2">
+                    @foreach ($products as $product)
+                    <div
+                        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="{{url('no-image.png')}}" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    {{$product->name}}</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$product->description}}
+                            </p>
+                            <p class="mb-3 text-white font-medium text-lg">R$ {{$product->price}}</p>
+                            <a href="#"
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Saiba mais
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
             </div>
+
         </div>
     </div>
-</x-guest-layout>
+</body>
+
+</html>
